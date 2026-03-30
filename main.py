db8acae0a8e731517f5fea30272340a2eee35a08
Iaanzen <iaaanzen@gmail.com>
2026-03-30 19:35:08 -0300

    Adicionado: NPC, caixa de diálogo, opção de escolhas

--- main.py
+++ main.py
@@ -16,6 +16,52 @@ gravidade = 0.5
 no_chao = False
 velocidade = 5
 
+#NPC
+class NPC:
+    def __init__(self, x, y, w=40, h=60, dialogos=[], escolhas=[]):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.dialogos = dialogos
+        self.fala_atual = 0
+        self.escolhas = escolhas
+        self.escolha_atual = 0
+
+    def desenhar(self, tela, camera_x):
+        pygame.draw.rect(tela, (100, 200, 100), (self.x - camera_x, self.y, self.w, self.h ))
+
+    def ver_interacao(self, rect_balin):
+        area = pygame.Rect(self.x - 40, self.y, self.w + 40, self.h)
+        return area.colliderect(rect_balin)
+
+
+
+npc1 = NPC(400, 360, dialogos=[
+    "Cavaleiro, preciso da sua ajuda...",
+    "Dizem que minha filha está amaldiçoada.",
+    "Você toparia resolver isso por mim?"
+], escolhas = [
+    "Sim, farei o serviço",
+    "Não posso fazer isso"
+])
+
+#Caixa de diálogo
+
+def desenhar_dialogo(tela, texto):
+    pygame.draw.rect(tela, (100, 200, 255), (0, 300, 800, 200))
+    fonte = pygame.font.SysFont(None, 32)
+    texto_render = fonte.render(texto, True, (255, 255, 255))
+    tela.blit(texto_render, (20, 330))
+
+def desenhar_escolhas(tela, escolhas, escolha_atual):
+    fonte = pygame.font.SysFont(None, 32)
+    for i, escolha in enumerate(escolhas):
+        cor = (255, 255, 0) if i == escolha_atual else (255, 255, 255)
+        texto_render = fonte.render(escolha, True, (cor))
+        tela.blit(texto_render, (20, 370 + i * 35))
+
+
 #mapa
 largura_mapa = 3000
 
@@ -30,33 +76,52 @@ plataformas = [
 
 
 rodando = True
+em_dialogo = False
+fim_dialogo = False
 
 while rodando:
+    # 1. Ler eventos
+    rect_balin = pygame.Rect(x, y, largura, altura)
     # 1. Ler eventos
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
         if evento.type == pygame.KEYDOWN:
-            if evento.key == pygame.K_UP and no_chao:
-                vel_y = -12
+            if not em_dialogo:
+                if evento.key == pygame.K_UP and no_chao:
+                    vel_y = -12
+                    fim_dialogo = False
+            if npc1.fala_atual == len(npc1.dialogos) - 1:
+                if evento.key == pygame.K_DOWN:
+                    npc1.escolha_atual += 1
+                if evento.key == pygame.K_UP:
+                    npc1.escolha_atual -= 1
+            if evento.key == pygame.K_SPACE:
+                if npc1.fala_atual < len(npc1.dialogos) - 1:
+                    npc1.fala_atual += 1
+                else:
+                    npc1.fala_atual = 0
+                    em_dialogo = False
+                    fim_dialogo = True
 
     # 2. Atualizar estado
-    teclas = pygame.key.get_pressed()
-    if teclas[pygame.K_LEFT]:
-        vel_x = -velocidade
-    elif teclas[pygame.K_RIGHT]:
-        vel_x = velocidade
-    else:
-        vel_x = 0
+    if not em_dialogo:
+        teclas = pygame.key.get_pressed()
+        if teclas[pygame.K_LEFT]:
+            vel_x = -velocidade
+        elif teclas[pygame.K_RIGHT]:
+            vel_x = velocidade
+        else:
+            vel_x = 0
 
-    vel_y += gravidade  # gravidade puxando pra baixo
-
+    vel_y += gravidade
     x += vel_x
     y_anterior = y
     y += vel_y
 
-
     rect_balin = pygame.Rect(x, y, largura, altura)
+
+
 
     no_chao = False
 
@@ -80,7 +145,17 @@ while rodando:
     # 3. Desenhar tela
     camera_x = x - 400
     tela.fill((30, 30, 30))  # fundo cinza escuro
+
+    if npc1.ver_interacao(rect_balin) and not fim_dialogo:
+        em_dialogo = True
+        vel_x = 0
+        desenhar_dialogo(tela, npc1.dialogos[npc1.fala_atual])
+        if npc1.fala_atual == len(npc1.dialogos) - 1:
+            desenhar_escolhas(tela, npc1.escolhas, npc1.escolha_atual)
+    else:
+        em_dialogo = False
     pygame.draw.rect(tela, (100, 200, 100), (x - camera_x, y, largura, altura))
+    npc1.desenhar(tela, camera_x, )
     for plataforma in plataformas:
         pygame.draw.rect(tela, (150, 75, 0), (plataforma[0] - camera_x, plataforma[1], plataforma[2], plataforma[3]))
     pygame.display.flip()
